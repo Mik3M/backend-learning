f7d192cfaa51912cb76d1c05ed73c9da675aa229
Mike <mikemonroy1205@gmail.com>
2025-10-28 14:10:12 -0500

    Actualización ejercicios día 2

--- ejercicios-dia2.js
+++ ejercicios-dia2.js
@@ -4,9 +4,9 @@ if (edad>=18){
     console.log("Eres mayor de edad");
 } else {
     console.log("Eres menor de edad");
-}*/   
+}   
 //Bucles
-/*for (let i=1; i<=3; i++){
+for (let i=1; i<=3; i++){
     console.log(`Vuelve número ${i}`);
 }
 
@@ -14,14 +14,35 @@ let x=0;
 while (x<2){
     console.log(`x vale: ${x}`);
     x++;
-}*/
+}
 //funciones
-/*const sumar = (a, b) => a + b;
+const sumar = (a, b) => a + b;
 const esPar= (num) => num % 2 === 0;
 
 console.log("Suma:", sumar(5, 7));
-console.log("Es par 8?", esPar(8));*/
+console.log("Es par 8?", esPar(8));
 
-const calcularAreaRectangulo = (base, altura) => (base * altura)/2;
-console.log(calcularAreaRectangulo (6, 6));
+//tareas
+1**
+const edad =25;
+if (edad>=18){
+    console.log("Eres mayor de edad");
+} else {
+    console.log("Eres menor de edad");
+}
+2**
+const esPar= (num) => num % 2 === 0;
+console.log("es par?", esPar(4));
+3**
+const valor = {};
+if (Object.keys(valor).length ===0) {
+    console.log("esta vacio");
+} else {
+    console.log("tiene elementos");
+}
+4**
 
+*/
+for (let i=1; i<=10; i++){
+    console.log(`Vuelve número ${i}`);
+}
